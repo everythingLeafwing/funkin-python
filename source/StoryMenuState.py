0dd9a2eea7582cf29d5447f6af5abaa69d1b2322
everythingLeafwing <77504737+everythingLeafwing@users.noreply.github.com>
2021-03-21 18:18:39 +0100

    add some assets for the story menu

--- source/StoryMenuState.py
+++ source/StoryMenuState.py
@@ -5,9 +5,9 @@ from source.images import menuBG, Alphabet, menu
 scr = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
 
 def Screen(screen, frame):
-    screen.fill((255, 210, 80))
+    screen.fill((0, 0, 0))
 
     pygame.draw.rect(screen, (0, 0, 0), (0, 0, 1920, 50))
-    pygame.draw.rect(screen, (0, 0, 0), (0, 450, 1920, 500))
+    pygame.draw.rect(screen, (255, 210, 80), (0, 50, 1920, 500))
 
     pygame.display.update()
