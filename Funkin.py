caa17c954c8c1cbe8c03e1cd3af165c4e89ecff7
everythingLeafwing <77504737+everythingLeafwing@users.noreply.github.com>
2021-03-21 19:03:27 +0100

    add the basic weeks to story and start to add difficulty selection

--- Funkin.py
+++ Funkin.py
@@ -26,6 +26,8 @@ songPlating = "none"
 
 MainMenuHighlighted = "Story"
 
+storyMenuEffector = 0
+
 # while the game is running
 while running:
     # for every event in the current events
@@ -55,20 +57,25 @@ while running:
                     currentScreen = "story menu"
 
             # if the events key is down
-            if event.key == pygame.K_DOWN and currentScreen == "menu":
+            if event.key == pygame.K_DOWN:
+                # if the current screen is the main menu
+                if currentScreen == "menu":
+                    # if the story button is highlighted
+                    if MainMenuHighlighted == "Story":
+                        # highlight the freeplay button
+                        MainMenuHighlighted = "Freeplay"
+                    # if the freeplay button is highlighted
+                    elif MainMenuHighlighted == "Freeplay":
+                        # highlight the donate button
+                        MainMenuHighlighted = "Donate"
+                    # if the donate button is highlighted
+                    elif MainMenuHighlighted == "Donate":
+                        # highlight the story button
+                        MainMenuHighlighted = "Story"
 
-                # if the story button is highlighted
-                if MainMenuHighlighted == "Story":
-                    # highlight the freeplay button
-                    MainMenuHighlighted = "Freeplay"
-                # if the freeplay button is highlighted
-                elif MainMenuHighlighted == "Freeplay":
-                    # highlight the donate button
-                    MainMenuHighlighted = "Donate"
-                # if the donate button is highlighted
-                elif MainMenuHighlighted == "Donate":
-                    # highlight the story button
-                    MainMenuHighlighted = "Story"
+                # if the current screen is the story menu
+                if currentScreen == "story menu":
+                    storyMenuEffector -= 40
 
             # if the up key is pressed
             if event.key == pygame.K_UP and currentScreen == "menu":
@@ -98,7 +105,7 @@ while running:
 
     if currentScreen == "story menu":
         # display the main menu screen in the current screen and frame
-        StoryMenuState.Screen(screen, frame)
+        StoryMenuState.Screen(screen, frame, storyMenuEffector)
 
     # increase the current "frame"
     frame += 0.06
--- source/StoryMenuState.py
+++ source/StoryMenuState.py
@@ -1,11 +1,24 @@
 import pygame
 from CalcEngine import Modulo, Floor
-from source.images import menuBG, Alphabet, menu
+from source.images import menuBG, Alphabet, menu, storyMenu
 
 scr = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
 
-def Screen(screen, frame):
+def Screen(screen, frame, UIeffector):
     screen.fill((0, 0, 0))
+
+    weekSpacing = 155
+
+    pygame.draw.rect(screen, (0, 0, 65), (0, 550, 1100, 50))
+    pygame.draw.rect(screen, (0, 0, 65), (1370, 550, 150, 50))
+
+    screen.blit(storyMenu.StoryMenuUI.tutorial[0], (525, 600 + UIeffector))
+    screen.blit(storyMenu.StoryMenuUI.week1[0], (525, 600 + UIeffector + (weekSpacing * 1)))
+    screen.blit(storyMenu.StoryMenuUI.week2[0], (525, 600 + UIeffector + (weekSpacing * 2)))
+    screen.blit(storyMenu.StoryMenuUI.week3[0], (525, 600 + UIeffector + (weekSpacing * 3)))
+    screen.blit(storyMenu.StoryMenuUI.week4[0], (525, 600 + UIeffector + (weekSpacing * 4)))
+    screen.blit(storyMenu.StoryMenuUI.week5[0], (525, 600 + UIeffector + (weekSpacing * 5)))
+    screen.blit(storyMenu.StoryMenuUI.week6[0], (525, 600 + UIeffector + (weekSpacing * 6)))
 
     pygame.draw.rect(screen, (0, 0, 0), (0, 0, 1920, 50))
     pygame.draw.rect(screen, (255, 210, 80), (0, 50, 1920, 500))
