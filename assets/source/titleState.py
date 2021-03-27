6bbeea2b5135772f4c088dca46a8816d87bfcded
everythingLeafwing <77504737+everythingLeafwing@users.noreply.github.com>
2021-03-27 08:24:02 +0100

    uhhh

--- assets/source/titleState.py
+++ assets/source/titleState.py
@@ -12,8 +12,15 @@ def Screen(screen, frame):
 
     screen.fill((0, 0, 0))
 
+    w, h = screen.get_size()
+
     screen.blit(gfDanceFrames[Floor(Modulo(frame, 27))], (1920 - 1080, 250))
-    screen.blit(titleEnterFrames[Floor(Modulo(frame, 42))], (115, 758))
+
+    size = (Floor(w * 0.9), Floor(h * 0.1))
+    titleEnter = pygame.transform.scale(titleEnterFrames[Floor(Modulo(frame, 42))], size)
+
+    screen.blit(titleEnter, ((w / 2) - Floor(w * 0.45), h - (h * 0.15)))
+
     screen.blit(logoBumpinFrames[Floor(Modulo(frame, 12))], (-35, -35))
 
     frame += 0.06
